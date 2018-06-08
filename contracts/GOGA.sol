be58980ee5edc99420bd70c972426b0f080f5fdb
wangjm <12301107@bjtu.edu.cn>
2018-06-08 18:49:33 +0800

    GOGA

--- contracts/GOGA.sol
+++ contracts/GOGA.sol
@@ -14,9 +14,19 @@ contract GOGA is ERC721,GOGBoardAccessor {
 
     mapping(uint256 => Asset) assetInfo;
     mapping(uint256 => uint256[]) projectToAsset;
+    mapping(uint256 => uint256) assetToProject;
+    mapping(uint256 => uint256) assetToProjectIndex;
 
-    function createAsset(uint256 project) public {
-
+    function createAsset(uint256 projectId, string _name) public {
+      uint256 tokenId = totalSupply();
+      _mint(msg.sender, tokenId);
+      uint length = projectToAsset[projectId].push(tokenId);
+      assetToProjectIndex[tokenId] = length.sub(1);
+      assetToProject[tokenId] = projectId;
+      Asset memory asset = Asset({
+        name: _name
+      });
+      assetInfo[tokenId] = asset;
     }
 
 }
