752ef4d23015a3fbfe79280243ce5f8b1d3693e7
wangjm <12301107@bjtu.edu.cn>
2018-06-13 10:30:11 +0800

    exchange

--- test/2_Exchange.js
+++ test/2_Exchange.js
@@ -1,4 +1,5 @@
 const Exchange = artifacts.require("./Exchange.sol");
+const GOGT = artifacts.require("./GOGT.sol");
 const AssertJump = require("./assert_jump.js");
 
 contract('Exchange', async (accounts) => {
@@ -11,12 +12,29 @@ contract('Exchange', async (accounts) => {
 
   it("should contract buy GOGT correctly", async () => {
     const _contract = await Exchange.deployed();
-    const _address = accounts[2];
-    let before = await web3.eth.getBalance(accounts[2]);
-    console.log(web3.fromWei(before,"ether").toString());
-    const result = await _contract.buyGOGT({from:accounts[2], value: web3.toWei(1, "ether")});
-    let initialContractBalance = await web3.eth.getBalance(_contract.address);
-    console.log(web3.fromWei(initialContractBalance,"ether").toString());
-    console.log(web3.fromWei(initialContractBalance,"ether").toString());
+    const _gogTContract = await GOGT.deployed();
+    const _gogTValue = await _gogTContract.balanceOf.call(accounts[2]);
+    assert.equal(0,_gogTValue);
+    const _result = await _contract.buyGOGT({from:accounts[2], value: web3.toWei(1, "ether")});
+    const _event = _result.logs[0].event;
+    assert.equal("BuyGOGT",_event);
+    const _initialContractBalance = await web3.eth.getBalance(_contract.address);
+    assert.equal(1,web3.fromWei(_initialContractBalance,"ether").toString());
+    const _gogTValue1 = await _gogTContract.balanceOf.call(accounts[2]);
+    assert.equal(_gogTValue1 > 0, true);
+  });
+
+  it("should contract sell GOGT correctly", async () => {
+    const _contract = await Exchange.deployed();
+    const _gogTContract = await GOGT.deployed();
+    const _gogTValue = await _gogTContract.balanceOf.call(accounts[2]);
+    assert.equal(_gogTValue > 0, true);
+    const _result = await _contract.sellGOGT(_gogTValue, {from:accounts[2]});
+    const _event = _result.logs[0].event;
+    assert.equal("SellGOGT",_event);
+    const _initialContractBalance = await web3.eth.getBalance(_contract.address);
+    assert.equal(1 > web3.fromWei(_initialContractBalance,"ether").toString(),true);
+    const _gogTValue1 = await _gogTContract.balanceOf.call(accounts[2]);
+    assert.equal(_gogTValue1,0);
   });
 });
